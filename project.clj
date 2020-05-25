f710d99ac67788528891dc59f174fa0916f46a22
Vale <valerauko@gmail.com>
2020-05-25 12:40:23 +0900

    Add tools.logging dep

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.logging "1.1.0"
+                  :exclusions [org.clojure/clojure]]
                  [metosin/jsonista "0.2.6"]
                  [io.netty/netty-common "4.1.50.Final"]
                  [io.netty/netty-buffer "4.1.50.Final"]
