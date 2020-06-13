5fc98209cf90c9c92c414dfc93d7cc93f1e1ad47
Vale <valerauko@gmail.com>
2020-06-13 15:49:28 +0900

    10.2 is necessary for building for graal
    TODO: update once it's not alpha

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [org.clojure/tools.logging "1.1.0"
                   :exclusions [org.clojure/clojure]]
                  [potemkin "0.4.5"]
