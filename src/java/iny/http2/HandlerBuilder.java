e9f4d7259f5a973765b1fbdd0fd2b436d174b331
Vale <valerauko@gmail.com>
2020-10-14 13:33:48 +0900

    explain why it's java

--- src/java/iny/http2/HandlerBuilder.java
+++ src/java/iny/http2/HandlerBuilder.java
@@ -12,6 +12,13 @@ import clojure.lang.IFn;
 
 import static io.netty.handler.logging.LogLevel.INFO;
 
+/*
+// builder for http2 connection handlers
+// HACK: the only reason this is in java is that overriding protected methods
+// that have the same name but different arity with clojure's proxy simply
+// does not work in a way necessary for this (overridden 0-ary build ->
+// buildfromconnection -> overridden 3-ary build)
+*/
 public final class HandlerBuilder
         extends AbstractHttp2ConnectionHandlerBuilder<Http2ConnectionHandler, HandlerBuilder> {
 
