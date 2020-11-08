d4325abff16e479c325849365d0d4405fc7d2130
Vale <valerauko@gmail.com>
2020-11-08 15:50:25 +0900

    Suggest adding dependency if not available

--- src/clj/iny/native.clj
+++ src/clj/iny/native.clj
@@ -38,6 +38,33 @@
        ;; it might be explicitly turned off
        (eval `(io.netty.channel.kqueue.KQueue/isAvailable))))))
 
+(defn suggest-package
+  ([]
+   (let [os (PlatformDependent/normalizedOs)
+         arch (PlatformDependent/normalizedArch)
+         clsfn #(when (% arch) (str os "-" arch))]
+     (case os
+       "linux"
+       (when-not (epoll?)
+         (let [classifier (clsfn #{"x86_64" "aarch64"})]
+           (suggest-package "epoll" classifier)))
+       ("darwin" "mac")
+       (when-not (kqueue?)
+         (let [classifier (clsfn #{"x86_64"})]
+           (suggest-package "kqueue" classifier)))
+       ("bsd")
+       (when-not (kqueue?)
+         (suggest-package "kqueue" nil))
+       nil)))
+  ([name classifier]
+   (let [netty-version (version-of "netty-transport")]
+     (log/debug
+      (str "Your system likely supports '" name "'. "
+           "For better native performance, try adding "
+           "`[io.netty/netty-transport-native-" name " \"" netty-version "\""
+           (when classifier (str " :classifier \"" classifier "\""))
+           "]` to your dependencies.")))))
+
 (defn ^MultithreadEventLoopGroup event-loop
   [^long thread-count]
   (cond
@@ -50,6 +77,8 @@
 
 (defn ^Class socket-chan
   []
+  (when-not (or (epoll?) (kqueue?))
+    (suggest-package))
   (cond
     (epoll?)
       (eval `io.netty.channel.epoll.EpollServerSocketChannel)
