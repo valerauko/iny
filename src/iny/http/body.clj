793c012eeb603976f87052a186939611ba9cc471
Vale <valerauko@gmail.com>
2021-05-13 10:41:09 +0900

    Indent auto-fix

--- src/iny/http/body.clj
+++ src/iny/http/body.clj
@@ -23,24 +23,24 @@
     (Class/forName "[B")
     (->buffer
       ([b]
-        (Unpooled/copiedBuffer ^bytes b))
+       (Unpooled/copiedBuffer ^bytes b))
       ([b ^ChannelHandlerContext ctx]
-        (doto (-> ctx (.alloc) (.ioBuffer (alength ^bytes b)))
-              (.writeBytes ^bytes b))))
+       (doto (-> ctx (.alloc) (.ioBuffer (alength ^bytes b)))
+             (.writeBytes ^bytes b))))
 
     nil
     (->buffer
       ([_]
-        Unpooled/EMPTY_BUFFER)
+       Unpooled/EMPTY_BUFFER)
       ([_ _]
-        Unpooled/EMPTY_BUFFER))
+       Unpooled/EMPTY_BUFFER))
 
     String
     (->buffer
       ([str]
-        (Unpooled/copiedBuffer ^String str charset))
+       (Unpooled/copiedBuffer ^String str charset))
       ([str ctx]
-        (->buffer ^bytes (.getBytes str) ctx)))
+       (->buffer ^bytes (.getBytes str) ctx)))
 
     ByteBuf
     (->buffer
